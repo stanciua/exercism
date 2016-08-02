2ebac88c926360a0e71fe8a00073a758329d12de
Andrei Stanciu <andreistanciu.as@gmail.com>
2016-08-02 03:02:04 -0500

    Updated the Parallel Letter Frequency to use a Scoped Thread Pool

--- rust/parallel-letter-frequency/src/lib.rs
+++ rust/parallel-letter-frequency/src/lib.rs
@@ -1,35 +1,44 @@
-extern crate rayon;
-
+extern crate scoped_threadpool;
 use std::collections::HashMap;
-use rayon::prelude::*;
+use std::sync::{Arc, Mutex};
+use scoped_threadpool::Pool;
 
-pub fn frequency(text: &[&str], _num_threads: usize) -> HashMap<char, usize> {
-    let letter_count = text.par_iter()
-        .map(|line| {
-            line.chars().filter(|ch| ch.is_alphabetic()).fold(HashMap::new(), |mut acc, val| {
+pub fn frequency(text: &[&str], num_threads: usize) -> HashMap<char, usize> {
+    let rslt = Arc::new(Mutex::new(HashMap::<char, usize>::new()));
+    let mut pool = Pool::new(num_threads as u32);
+
+    pool.scoped(|scope| {
+        for line in text {
+            let rslt = rslt.clone();
+            scope.execute(move || {
+                let rslt_per_line = line.chars()
+                    .filter(|ch| ch.is_alphabetic())
+                    .fold(HashMap::new(), |mut acc, val| {
+                        {
+                            let count = acc.entry(val.to_lowercase().next().unwrap()).or_insert(0);
+                            *count += 1;
+                        }
+                        acc
+                    });
+
                 {
-                    let count = acc.entry(val.to_lowercase().next().unwrap()).or_insert(0);
-                    *count += 1;
+                    let rslt = rslt.lock().unwrap();
+                    let _ = rslt_per_line.iter().fold(rslt, |mut acc, (key, val)| {
+                        {
+                            let new_val = acc.entry(*key).or_insert(0);
+                            *new_val += *val;
+                        }
+                        acc
+                    });
                 }
-                acc
-            })
-        })
-        .reduce_with(|acc, x| {
-            x.iter().fold(acc, |mut acc, (key, val)| {
-                {
-                    let new_val = acc.entry(*key).or_insert(0);
-                    *new_val += *val;
-                }
-                acc
-            })
-        });
 
-    if let Some(rslt) = letter_count {
-        println!("{:?}", rslt);
-        rslt
-    } else {
-        HashMap::new()
-    }
+            });
+        }
 
+
+    });
+
+    let data = rslt.lock().unwrap();
+    data.clone().into_iter().collect()
 
 }
