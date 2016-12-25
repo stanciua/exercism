aa56425b99be018ac6a8f5a929cb86f3102f921f
Andrei Stanciu <andreistanciu.as@gmail.com>
2016-12-25 19:28:03 +0200

    Modified Bob exercise

--- scala/bob/src/main/scala/Bob.scala
+++ scala/bob/src/main/scala/Bob.scala
@@ -3,10 +3,12 @@ class Bob {
     val alphaChars = ('a' to 'z').toSet | ('A' to 'Z').toSet
     !(phrase.toSet & alphaChars).isEmpty
   }
+
+  private def isYelling(phrase: String) : Boolean = phrase == "" || (phrase map (_ isWhitespace) forall (_ == true))  
   
   def hey(phrase: String) : String = {
     phrase match {
-      case ph if ph == "" || (ph map (_ isWhitespace) forall (_ == true)) => "Fine. Be that way!"
+      case ph if isYelling(phrase) => "Fine. Be that way!"
       case ph if isAlpha(phrase) && ph.toUpperCase == ph => "Whoa, chill out!"
       case ph if ph.endsWith("?") => "Sure."
       case _ => "Whatever."
