ab5eec2b731bc8f9eccf0b65c34a10899b77a266
Andrei Stanciu <andreistanciu.as@gmail.com>
2017-01-09 10:52:45 +0200

    Javascript: Hello World and Leap

---
+++ javascript/leap/leap.js
@@ -0,0 +1,12 @@
+class Leap {
+  constructor(year) {
+    this.year = year
+  }
+  isLeap() {
+    if (this.year % 400 === 0) return true  
+    if (this.year % 100 === 0) return false
+    if (this.year % 4 === 0) return true
+    return false
+  }
+}
+module.exports = Leap;
