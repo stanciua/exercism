2001e01d4a1981b43cb9e5a1594e43432b4bac0f
Andrei Stanciu <andreistanciu.as@gmail.com>
2017-01-16 10:30:31 +0200

    Javascript: Phone Number completed

---
+++ javascript/phone-number/phone-number.js
@@ -0,0 +1,34 @@
+const Immutable = require("Immutable")
+
+class PhoneNumber {
+    constructor(phone) {
+        this.phone = phone
+    }
+
+    number() {
+        const numbers = Immutable.Set.of(...
+            "0123456789");
+        let number = Immutable.List.of(...this.phone).filter(ch => numbers.has(ch));
+        const badNumber = "0000000000";
+        if (number.size < 10 || number.size > 12) return badNumber;
+        if (number.size === 11) {
+            if (number.first() === "1") {
+                number = number.rest()
+            } else {
+                return badNumber;
+            }
+        }
+        return number.join("");
+    }
+
+    areaCode() {
+        return Immutable.List.of(...this.number()).take(3).join("");
+    }
+
+    toString() {
+        let n = this.number();
+        return `(${this.areaCode()}) ${n.substr(3, 3)}-${n.substr(6, 4)}`
+    }
+}
+
+module.exports = PhoneNumber
