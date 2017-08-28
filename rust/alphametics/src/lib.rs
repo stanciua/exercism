c1ecbd74e2e4e2e6c0bde39b8a9688b2929fe1b0
stanciua <andrei.stanciu@aciworldwide.com>
2017-08-28 17:22:07 +0300

    rust: Some refactoring to speed up backtracking algo

--- rust/alphametics/src/lib.rs
+++ rust/alphametics/src/lib.rs
@@ -1,11 +1,11 @@
 extern crate itertools;
 
-use std::collections::{HashSet, HashMap};
+use std::collections::{HashMap, HashSet};
 use std::fmt;
 use itertools::Itertools;
 
 struct Puzzle<'a> {
-    all_tkns: Vec<String>,
+    left_to_right_data: &'a Vec<(String, char)>,
     all_chars: &'a str,
     char_dig: &'a mut Vec<(char, u8)>,
     leading_zeroes: &'a [char],
@@ -15,8 +15,8 @@ impl<'a> fmt::Debug for Puzzle<'a> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(
             f,
-            "all_tkns: {:?}, all_chars: {:?}, char_dig: {:?}, leading_zeroes: {:?}",
-            self.all_tkns,
+            "left_to_right_data: {:?}, all_chars: {:?}, char_dig: {:?}, leading_zeroes: {:?}",
+            self.left_to_right_data,
             self.all_chars,
             self.char_dig,
             self.leading_zeroes
@@ -26,13 +26,13 @@ impl<'a> fmt::Debug for Puzzle<'a> {
 
 impl<'a> Puzzle<'a> {
     pub fn new(
-        all_tkns: Vec<String>,
+        left_to_right_data: &'a Vec<(String, char)>,
         chars: &'a str,
         char_dig: &'a mut Vec<(char, u8)>,
         leading_zeroes: &'a [char],
     ) -> Puzzle<'a> {
         Puzzle {
-            all_tkns: all_tkns,
+            left_to_right_data: left_to_right_data,
             all_chars: chars,
             char_dig: char_dig,
             leading_zeroes: leading_zeroes,
@@ -53,85 +53,38 @@ impl<'a> Puzzle<'a> {
         true
     }
 
-    pub fn puzzle_solved(&self) -> bool {
-        return self.is_combination_alphametic();
-    }
-
     pub fn unassign_letter(&mut self, letter: char, digit: u8) {
-        if let Some(idx) = self.char_dig.iter().position(
-            |&(x, y)| x == letter && digit == y,
-        )
+        if let Some(idx) = self.char_dig
+            .iter()
+            .position(|&(x, y)| x == letter && digit == y)
         {
             self.char_dig.remove(idx);
         }
     }
-    pub fn find_solution(&mut self, letter_to_assign: &str, carry: i32) -> bool {
-        if letter_to_assign.len() == 0 {
-            return self.puzzle_solved();
-        }
 
-        for d in 0..10 {
-            if self.assign_letter(letter_to_assign.chars().take(1).next().unwrap(), d) {
-                if self.find_solution(&letter_to_assign[1..], 0) {
-                    return true;
-                }
-                self.unassign_letter(letter_to_assign.chars().take(1).next().unwrap(), d);
-            }
-        }
-        return false;
-    }
+    pub fn find_solution(&mut self, carry: i32) -> bool {
+        println!("puzzle: {:?}", *self);
+        // if letter_to_assign.len() == 0 {
+        //     return false;
+        // }
 
-    fn is_combination_alphametic(&self) -> bool {
-        let result = self.str_to_num(self.all_tkns.last().unwrap(), self.char_dig);
-        if result.is_none() {
-            return false;
-        }
-
-        let mut num = 0;
-        for token in &self.all_tkns[..self.all_tkns.len() - 1] {
-            let rslt = self.str_to_num(&*token, self.char_dig);
-            if rslt.is_none() {
-                return false;
-            }
-            num += rslt.unwrap();
-        }
-
-        result.unwrap() == num
-    }
-
-    fn str_to_num(&self, val: &str, lnums_slice: &[(char, u8)]) -> Option<u32> {
-        let mut num = 0;
-        let mut val_slice = val;
-        let letter_to_digit_map = lnums_slice.iter().cloned().collect::<HashMap<_, _>>();
-
-        if let Some(l) = val.chars().take(1).next() {
-            if l != '_' && letter_to_digit_map[&l] == 0 {
-                return None;
-            }
-        }
-        for letter in val.chars() {
-            let mut digit = 0;
-            if letter == '_' {
-                digit = 0;
-            } else {
-                digit = letter_to_digit_map[&letter];
-            }
-            num += 10u32.pow(val_slice.len() as u32 - 1) * digit as u32;
-            val_slice = &val_slice[1..];
-        }
-
-        Some(num)
+        // for d in 0..10 {
+        //     if self.assign_letter(letter_to_assign.chars().take(1).next().unwrap(), d) {
+        //         if self.find_solution(0) {
+        //             return true;
+        //         }
+        //         self.unassign_letter(letter_to_assign.chars().take(1).next().unwrap(), d);
+        //     }
+        // }
+        // return false;
+        false
     }
 }
 
-fn normalize_tokens(tkns: &mut [String]) {
-    let mut tkn_max_lgth = tkns[0].len();
-    if let Some(max) = tkns.iter_mut().max_by(|x, y| x.len().cmp(&y.len())) {
-        tkn_max_lgth = max.len();
-    }
+fn normalize_tokens(tkns: &mut [String], max_len: usize) {
     for tkn in tkns.iter_mut() {
-        if tkn.len() < tkn_max_lgth {
-            let mut padded_tkn = "_".repeat(tkn_max_lgth - tkn.len());
+        if tkn.len() < max_len {
+            let mut padded_tkn = "_".repeat(max_len - tkn.len());
             padded_tkn.extend(tkn.chars());
             *tkn = padded_tkn;
         }
@@ -170,15 +123,25 @@ pub fn solve(puzzle: &str) -> Option<HashMap<char, u8>> {
         .unique()
         .collect::<String>();
 
-    let mut all_tkns = left_tkns.iter().map(|x| x.to_string()).collect::<Vec<_>>();
-    all_tkns.push(result.to_string());
+    let mut left_tkns = left_tkns.iter().map(|x| x.to_string()).collect::<Vec<_>>();
 
-    normalize_tokens(all_tkns.as_mut_slice());
+
+    normalize_tokens(left_tkns.as_mut_slice(), result.len());
 
     let mut char_dig = Vec::new();
-    let mut puzzle = Puzzle::new(all_tkns, &all_chars, &mut char_dig, &leading_zero_letters);
+    let mut left_letters = &group_add_letter_togheter(&left_tkns);
 
-    if puzzle.find_solution(puzzle.all_chars, 0) {
+    let mut puzzle = Puzzle::new(
+        &left_letters
+            .iter()
+            .zip(result.chars())
+            .collect::<Vec<(_, _)>>(),
+        &all_chars,
+        &mut char_dig,
+        &leading_zero_letters,
+    );
+
+    if puzzle.find_solution(0) {
         return Some(
             puzzle
                 .char_dig
@@ -189,4 +152,19 @@ pub fn solve(puzzle: &str) -> Option<HashMap<char, u8>> {
     }
 
     None
+}
+
+fn group_add_letter_togheter<'a>(left_tkns: &'a [String]) -> Vec<String> {
+    let mut output = Vec::new();
+    for i in 0..left_tkns[0].len() {
+        let letters = left_tkns.iter().fold(String::new(), |mut acc, tkn| {
+            if let Some(l) = tkn.chars().skip(i).take(1).next() {
+                acc.push(l);
+            }
+            acc
+        });
+
+        output.push(letters);
+    }
+    output
 }
