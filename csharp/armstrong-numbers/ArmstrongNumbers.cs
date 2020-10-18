fbae29e9b448f846c6fd6b519200eedd91b74512
stanciua <andreistanciu.as@gmail.com>
2020-10-18 20:30:52 +0300

    Finished ArmstrongNumber exercise.

---
+++ csharp/armstrong-numbers/ArmstrongNumbers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public static class ArmstrongNumbers
+{
+    public static bool IsArmstrongNumber(int number)
+    {
+        int original = number;
+        var length = number.ToString().Length;
+        var calculated = 0;
+        var divider = (int)Math.Pow(10, (length - 1));
+        while (number != 0)
+        {
+            calculated += (int)Math.Pow(number / divider, length);
+            number %= divider;
+            divider /= 10;
+        }
+
+        return original == calculated;
+    }
+}
